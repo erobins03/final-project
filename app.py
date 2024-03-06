0d46271ded341f84dc023c76dc544dc5c9541c16
erobins03 <rainbowroserobins@gmail.com>
2024-03-06 12:27:17 -0800

    updating the project

--- app.py
+++ app.py
@@ -2,68 +2,69 @@ import urllib.parse, urllib.request, urllib.error, json
 import requests
 import pprint
 from flask import Flask, render_template, request
-import function
+from function import *
 
 app = Flask (__name__)
 
-def get_url(url):
-    try:
-        response = urllib.request.urlopen(url)
-        return json.loads(response.read())
-    except urllib.error.URLError as e:
-        print("Error: Unable to reach the server: ", e)
-        return None
-    except urllib.error.HTTPError as e:
-        print("Error: HTTP error code: ", e.code, e.reason)
-        return None
-    except Exception as e:
-        print("An unexpected error occurred: ", e)
-        return None
+# def get_url(url):
+#     try:
+#         response = urllib.request.urlopen(url)
+#         return json.loads(response.read())
+#     except urllib.error.URLError as e:
+#         # print("Error: Unable to reach the server: ", e)
+#         return None
+#     except urllib.error.HTTPError as e:
+#         # print("Error: HTTP error code: ", e.code, e.reason)
+#         return None
+#     except Exception as e:
+#         # print("An unexpected error occurred: ", e)
+#         return None
+#
+# def print_data(image_data):
+#     if image_data and image_data["status"] == "success":
+#         image_url = image_data["message"]
+#         # print("Random dog image url: ", image_url)
+#         return False
+#     else:
+#         # print("Failed to get a random dog image.")
+#         return True
+#
+#
+# def get_dog_photo(breed):
+#     baseurl = "https://dog.ceo/api/breed/"
+#     endpoint = "/images/random"
+#
+#     def make_breed(breed):
+#         if len(breed.split()) == 2:
+#             dog = breed.split()[1]
+#             dog += '/'
+#             dog += breed.split()[0]
+#             return dog
+#         else:
+#             return breed
+#
+#     def make_url(dog):
+#         return baseurl + dog + endpoint
+#
+#     dog = make_breed(breed)
+#     output = get_url(make_url(dog))
+#     failure = print_data(output)
+#     if failure:
+#         output = get_url(make_url(breed.replace(' ', '')))
+#         failure = print_data(output)
+#     if failure and len(breed.split()) == 2:
+#         split = breed.split()
+#         breed = split[1] + ' ' + split[0]
+#         dog = make_breed(breed)
+#         url = make_url(dog)
+#         output = get_url(url)
+#         print_data(output)
+#     return output
+#
+# def get_dog_info_url(breed):
+#     dog_info_url = "https://en.wikipedia.org/wiki/" + breed.replace(" ", "_")
+#     return dog_info_url
 
-def print_data(image_data):
-    if image_data and image_data["status"] == "success":
-        image_url = image_data["message"]
-        print("Random dog image url: ", image_url)
-        return False
-    else:
-        print("Failed to get a random dog image.")
-        return True
-
-
-def get_dog_photo(breed):
-    baseurl = "https://dog.ceo/api/breed/"
-    endpoint = "/images/random"
-
-    def make_breed(breed):
-        if len(breed.split()) == 2:
-            dog = breed.split()[1]
-            dog += '/'
-            dog += breed.split()[0]
-            return dog
-        else:
-            return breed
-
-    def make_url(dog):
-        return baseurl + dog + endpoint
-
-    dog = make_breed(breed)
-    output = get_url(make_url(dog))
-    failure = print_data(output)
-    if failure:
-        output = get_url(make_url(breed.replace(' ', '')))
-        failure = print_data(output)
-    if failure and len(breed.split()) == 2:
-        split = breed.split()
-        breed = split[1] + ' ' + split[0]
-        dog = make_breed(breed)
-        url = make_url(dog)
-        output = get_url(url)
-        print_data(output)
-    return output
-
-def get_dog_info_url(breed):
-    dog_info_url = "https://en.wikipedia.org/wiki/" + breed.replace(" ", "_")
-    return dog_info_url
 
 @app.route("/")
 def index():
@@ -74,11 +75,12 @@ def results():
     if request.method == "POST":
         breed = request.form["breed"]
         image_data = get_dog_photo(breed)
-        # if print_data(image_data):
-        #     image_data = get_dog_photo(breed.replace(' ', ''))
-        #     print_data(image_data)
+        if image_data is None:
+            random_image_data = get_url("https://dog.ceo/api/breeds/image/random")
+            return render_template("results.html", breed=breed, image_data=random_image_data, success=False)
         dog_info_url = get_dog_info_url(breed)
-        return render_template("results.html", breed=breed, image_data=image_data, dog_info_url=dog_info_url)
+        return render_template("results.html", breed=breed, image_data=image_data, dog_info_url=dog_info_url, success=True)
+
 
     if request.method == "GET":
         return index()
--- function.py
+++ function.py
@@ -1,65 +1,60 @@
-# import urllib.parse, urllib.request, urllib.error, json
-# import requests
-# import pprint
-# from flask import Flask, render_template, request
-#
-#
-# # # Create an instance of Flask
-# # app = Flask (__name__)
-# #
-# # # Create a view function for /
-# # @app.route("/")
-# # def index():
-# #     return render_template("index.html")
-# #
-# # @app.route("/info")
-# def get_url(url):
-#     try:
-#         response = urllib.request.urlopen(url)
-#         return json.loads(response.read())
-#     except urllib.error.URLError as e:
-#         print("Error: Unable to reach the server: ", e)
-#         return None
-#     except urllib.error.HTTPError as e:
-#         print("Error: HTTP error code: ", e.code, e.reason)
-#         return None
-#     except Exception as e:
-#         print("An unexpected error occurred: ", e)
-#         return None
-#
-# def get_dog_photo(breed):
-#     baseurl = "https://dog.ceo/api/breed/"
-#     endpoint = "/images/random"
-#
-#     if len(breed.split()) == 2:
-#         dog = breed.split()[1]
-#         dog += '/'
-#         dog += breed.split()[0]
-#     else:
-#         dog = breed
-#
-#     url = baseurl + dog + endpoint
-#     return get_url(url)
-#
-# def print_data(image_data):
-#     if image_data and image_data["status"] == "success":
-#         image_url = image_data["message"]
-#         print("Random dog image url: ", image_url)
-#     else:
-#         print("Failed to get a random dog image.")
-#
-# dog_breeds = ["fox terrier", "husky", "golden retriever", "chesapeake retriever", "french bulldog"]
-#
-# for breed in dog_breeds:
-#     image_data = get_dog_photo(breed)
-#     print_data(image_data)
-#
-#     dog_info = "https://en.wikipedia.org/wiki/"
-#     dog_info += breed.replace(" ", "_")
-#
-#     name = requests.get(dog_info)
-#     # print(name.status_code)
-#     if name.status_code == 404:
-#         print("This Wikipedia page does not exist" + "\n")
-#     elif name.status_code == 200:
-#         print("Here is some information about the dog: " + dog_info + "\n")
+import urllib.parse, urllib.request, urllib.error, json
+
+def get_url(url):
+    try:
+        response = urllib.request.urlopen(url)
+        return json.loads(response.read())
+    except urllib.error.URLError as e:
+        # print("Error: Unable to reach the server: ", e)
+        return None
+    except urllib.error.HTTPError as e:
+        # print("Error: HTTP error code: ", e.code, e.reason)
+        return None
+    except Exception as e:
+        # print("An unexpected error occurred: ", e)
+        return None
+
+def print_data(image_data):
+    if image_data and image_data["status"] == "success":
+        image_url = image_data["message"]
+        # print("Random dog image url: ", image_url)
+        return False
+    else:
+        # print("Failed to get a random dog image.")
+        return True
+
+
+def get_dog_photo(breed):
+    baseurl = "https://dog.ceo/api/breed/"
+    endpoint = "/images/random"
+
+    def make_breed(breed):
+        if len(breed.split()) == 2:
+            dog = breed.split()[1]
+            dog += '/'
+            dog += breed.split()[0]
+            return dog
+        else:
+            return breed
+
+    def make_url(dog):
+        return baseurl + dog + endpoint
+
+    dog = make_breed(breed)
+    output = get_url(make_url(dog))
+    failure = print_data(output)
+    if failure:
+        output = get_url(make_url(breed.replace(' ', '')))
+        failure = print_data(output)
+    if failure and len(breed.split()) == 2:
+        split = breed.split()
+        breed = split[1] + ' ' + split[0]
+        dog = make_breed(breed)
+        url = make_url(dog)
+        output = get_url(url)
+        print_data(output)
+    return output
+
+def get_dog_info_url(breed):
+    dog_info_url = "https://en.wikipedia.org/wiki/" + breed.replace(" ", "_")
+    return dog_info_url
